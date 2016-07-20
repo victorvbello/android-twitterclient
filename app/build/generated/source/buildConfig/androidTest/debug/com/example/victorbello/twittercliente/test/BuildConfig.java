b7e02a04458ca9bc4a876edf05d03756e68e91cc
Victor Bello <Victor Bello@localhost>
2016-07-20 15:37:33 -0400

    Inicio

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/victorbello/twittercliente/test/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.victorbello.twittercliente.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.victorbello.twittercliente.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String TWITTER_KEY = "uk3mGoWAwGt0uKv9EtD9s0fLt";
+  public static final String TWITTER_SECRET = "inKivA0bnRXr0kjn3Y3nLIDjHVW2EYoHXdQjzjSaNHHuq7XZZg";
+}
